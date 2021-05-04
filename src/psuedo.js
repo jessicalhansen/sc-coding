67a42d32fca41e6edea526a3e429ed3afd4c8a30
jessicalh <jessicalynnehansen@gmail.com>
2021-05-03 23:46:52 -0600

    Initial setup complete.

---
+++ src/psuedo.js
@@ -0,0 +1,13 @@
+// Create a Shape base class with GetArea and GetName and IncreaseSize methods
+// Create a Square class that extends the Shape class with a default side length of 1
+// Create a Circle class that extends the Shape class with a default radius of 1
+// The GetArea method should return the area of the shape
+// The GetName method should return the name of the shape (“Square” or “Circle”)
+// The IncreaseSize method should take a percentage parameter and increase the size of the shape by that percentage
+// Create a collection of Shapes 
+// Insert a Square object into the Shapes collection with a side length of 3
+// Insert a Circle into the Shapes collection with a radius of 2
+// Use a for loop to do the following for each shape:
+// Print the name and area of the shape
+// Increase the size of the shape by 10%
+// Print the name and new area of the shape 

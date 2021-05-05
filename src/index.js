fad3e0f0e1991d00c40c5109970d76448644423f
jessicalh <jessicalynnehansen@gmail.com>
2021-05-05 06:21:11 -0600

    Created Square and Circle class, shape collection, and for loop.

--- src/index.js
+++ src/index.js
@@ -1,43 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Shapes from './Shape';
 import './index.css';
 
 class Shape extends React.Component {
-  // constructor() {
-  //     super();
-  // }
+  constructor(r) {
+    super()
+    this.r = r;
+    this.getArea = this.getArea.bind(this)
+    this.getName = this.getName.bind(this)
+    this.increaseSize = this.increaseSize.bind(this)
+  }
+  getName() {
+      return Object.getPrototypeOf(this).constructor.name;
+  }
 
   getArea() {
-      console.log('getArea method');
-  }
+    if (this.getName() === 'Circle') {
+      return (Math.PI * (this.r ** 2))
+    } else return (this.r ** 2)
+  };
 
-  getName() {
-      console.log('getName method');
+  increaseSize = (num) => {
+    const percent = num * .100
+    return this.getArea() + (this.getArea() * percent)
   }
-
-  increaseSize() {
-      console.log('increaseSize method');
-  }
-
 
   render() {
       return (
           <>
           <h2>Shapes</h2>
-          <div className="shapes">
-              <p>{this.getArea()}</p>
-          </div>
           </>
       )
   }
 }
 
-function App () {
+class Square extends Shape {
+  constructor(r) {
+    super(r);
+    
+  }
+  render() {
+    return (
+      <>
+      </>
+    )
+  }
+}
+
+class Circle extends Shape {
+  constructor(r) {
+    super(r);
+  }
+  render() {
+    return (
+      <>
+      </>
+    )
+  }
+}
+
+const shapes = [new Circle(2), new Square(3)]
+
+const circle = shapes[0];
+const square = shapes[1];
+
+for (let i = 1; i < 100; i++) {
+  console.log(circle.getArea(), circle.getName(), circle.increaseSize(i))
+}
+
+for (let i = 1; i < 100; i++) {
+    console.log(square.getArea(), square.getName(), square.increaseSize(i))
+}
+
+function App() {
   return (
     <div className="Main">
-      <h1>SC Coding Assessment</h1>
-        <Shape />
+      <h1>React OOP Shapes</h1>
     </div>
   )
 } 
